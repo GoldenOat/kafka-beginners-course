4b614a2c78795f1e308304922b40038b827b6b8d
Alexis Tang <alexistang0822@gmail.com>
2022-09-04 15:59:12 -0400

    kafka producer added

---
+++ kafka-basics/src/main/java/org/udemy/kafka/ProducerDemo.java
@@ -0,0 +1,33 @@
+package org.udemy.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+        log.info("hello world");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // transform java object to binary for kafka
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        // create a producer record
+
+
+        // send data - async
+
+        // flush and close the Producer
+    }
+}
